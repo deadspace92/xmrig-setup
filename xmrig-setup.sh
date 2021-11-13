1caeb063b00c4635c356591c475ac810d2b78662
deadspace92 <44837910+deadspace92@users.noreply.github.com>
2021-11-13 05:43:44 -0500

    Update xmrig-setup.sh

--- xmrig-setup.sh
+++ xmrig-setup.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+sudo apt install build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build
